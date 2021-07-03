0dea08ea8300f24857689ceaf18f778910ee97d6
mikeleonida <74788426+mikeleonida@users.noreply.github.com>
2021-07-03 12:06:25 -0400

    Initial commit

---
+++ components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>keeper app title</h1>
+      <p>This is Michael's wonderful keeper app. Enjoy at your own risk!</p>
+    </div>
+  );
+}
+
+export default Note;
